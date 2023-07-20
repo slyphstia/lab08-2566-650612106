0148a0ec24b3bf718ed533460a245bd9f288e12b
hghgh <apiwit_boonyarit@cmu.ac.th>
2023-07-20 10:35:30 +0700

    i forget to push to github sorry kub

---
+++ component/footer.jsx
@@ -0,0 +1,10 @@
+export const Footer = (props) => {
+  return (
+    <div className="mt-3">
+      <p className="text-secondary text-center">
+       
+        copyright © {props.year} {props.fullname} {props.studentid }
+      </p>
+    </div>
+  );
+};
